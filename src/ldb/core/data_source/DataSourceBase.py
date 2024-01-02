62158f3a24ba6e0aa5cbbc7678182266bb77011a
nuuuwan <nuuuwan@gmail.com>
2024-01-02 10:43:56 +0530

    Hide unused data items

--- src/ldb/core/data_source/DataSourceBase.py
+++ src/ldb/core/data_source/DataSourceBase.py
@@ -8,14 +8,14 @@ class DataSourceBase:
     source_id: str
     category: str
     sub_category: str
-    scale: str
-    unit: str
+    # scale: str
+    # unit: str
     frequency_name: str
-    i_subject: int
-    footnotes: dict
-    summary_statistics: dict
+    # i_subject: int
+    # footnotes: dict
+    # summary_statistics: dict
     cleaned_data: dict
-    raw_data: dict
+    # raw_data: dict
 
     @staticmethod
     def from_dict(data: dict):
@@ -23,13 +23,25 @@ class DataSourceBase:
             source_id=data['source_id'],
             category=data['category'],
             sub_category=data['sub_category'],
-            scale=data['scale'],
-            unit=data['unit'],
+            # scale=data['scale'],
+            # unit=data['unit'],
             frequency_name=data['frequency_name'],
-            i_subject=(int)(data['i_subject']),
-            footnotes=data['footnotes'],
-            summary_statistics=data['summary_statistics'],
+            # i_subject=(int)(data['i_subject']),
+            # footnotes=data['footnotes'],
+            # summary_statistics=data['summary_statistics'],
             cleaned_data=data['cleaned_data'],
-            raw_data=data['raw_data'],
+            # raw_data=data['raw_data'],
         )
-    
+
+    @property
+    def short_name(self) -> str:
+        return ' '.join(
+            [
+                self.category,
+                self.sub_category,
+                self.frequency_name,
+            ]
+        )
+
+    def __str__(self) -> str:
+        return f'DataSource({self.short_name})'
